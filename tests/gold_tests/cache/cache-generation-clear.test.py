951ef510a2fc32f9a19f98d3a71386a361768868
Jason Kenny <dragon512@live.com>
2016-09-30 17:14:52 -0500

    update a test

--- tests/gold_tests/cache/cache-generation-clear.test.py
+++ tests/gold_tests/cache/cache-generation-clear.test.py
@@ -5,7 +5,7 @@ Test that incrementing the cache generation acts like a cache clear
 '''
 # need Curl
 Test.SkipUnless(Condition.HasProgram("curl","Curl need to be installed on sytem for this test to work"))
-#Test.ContinueOnFail=True
+Test.ContinueOnFail=True
 # Define default ATS
 ts=Test.MakeATSProcess("ts",command="traffic_manager")
 
@@ -35,7 +35,7 @@ tr=Test.AddTestRun()
 tr.Processes.Default.Command='curl "http://127.0.0.1:{0}/default/cache/10/{1}" -H "x-debug: x-cache,x-cache-key,via,x-cache-generation" --verbose'.format(ts.Variables.port,objectid)
 tr.Processes.Default.ReturnCode=0
 # time delay as proxy.config.http.wait_for_cache could be broken
-tr.Processes.Default.StartBefore(Test.Processes.ts,ready=5)
+tr.Processes.Default.StartBefore(Test.Processes.ts,ready=10)
 tr.Processes.Default.Streams.All="gold/miss_default-1.gold"
 
 # Second touch is a HIT for default.
@@ -51,22 +51,20 @@ tr.Processes.Default.ForceUseShell=False
 tr.Processes.Default.ReturnCode=0
 tr.Processes.Default.Env=ts.Env # set the environment for traffic_control to run in
 
-# Call traffic_ctrl to load new data
-tr=Test.AddTestRun()
-tr.Processes.Default.Command='traffic_ctl reload'
-tr.Processes.Default.ReturnCode=0
-tr.Processes.Default.Env=ts.Env # set the environment for traffic_control to run in
-
 # new generation should first be a miss.
 tr=Test.AddTestRun()
 # create a new traffic_ctrl call and the environment 
-tr.Processes.Process("ctrl","traffic_ctl reload").Env=ts.Env 
+#tr.Processes.Process("ctrl","traffic_ctl config reload").Env=ts.Env
+#tr.Processes.ctrl.ReturnCode=0
+tr.Processes.Process("sleep","sleep 10").Env=ts.Env
+tr.Processes.sleep.ReturnCode=0
 tr.Processes.Default.Command='curl "http://127.0.0.1:{0}/default/cache/10/{1}" -H "x-debug: x-cache,x-cache-key,via,x-cache-generation" --verbose'.format(ts.Variables.port,objectid)
 tr.Processes.Default.ReturnCode=0
 tr.Processes.Default.Streams.All="gold/miss_default77.gold"
-# call traffic_ctrl reload with one second delay before running curl command 
-# to provide time for traffic_server to load cache data.
-tr.Processes.Default.StartBefore(tr.Processes.ctrl,ready=1) 
+# Setup order to allow traffic_ctrl to reload
+# and have a delay (10 secs) before we try to curl again
+#tr.Processes.sleep.StartBefore(tr.Processes.ctrl)
+tr.Processes.Default.StartBefore(tr.Processes.sleep) 
 
 # new generation should should now hit.
 tr=Test.AddTestRun()
