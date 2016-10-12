a00e2698c79422298a3c3ad4b6f7ca60386a834a
Persia Aziz <persia@yahoo-inc.com>
2016-10-12 17:26:07 -0500

    clean remap test

--- tests/gold_tests/remap/remap_http.test.py
+++ tests/gold_tests/remap/remap_http.test.py
@@ -10,15 +10,17 @@ Test.ContinueOnFail=True
 # Define default ATS
 ts=Test.MakeATSProcess("ts")
 server=Test.MakeOriginServer("server")
-#server.Bob("LOOK AT ME.....................***************************************************************************")
-request_header={"headers": "GET /remap HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
 
+#**testname is required**
+Test.testName = "remap"
+request_header={"headers": "GET /remap HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
+#expected response from the origin server
 response_header={"headers": "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
 
 f=server.Disk.File(os.path.join(server.Variables.DataDir,"persia.json"),id="data1",typename="ats:tracelog")
 
-f.addResponse("persia.json", "remap", request_header, response_header)
-ts.Setup.CopyAs("../persia.json",server.Variables.DataDir)
+#add response to the server dictionary
+server.addResponse("aziz.json", Test.testName, request_header, response_header)
 ts.Disk.records_config.update({
         'proxy.config.diags.debug.enabled': 1,
         'proxy.config.diags.debug.tags': 'url.*',
@@ -43,31 +45,31 @@ tr.StillRunningAfter=server
 
 # www.example.com host
 tr=Test.AddTestRun()
-tr.Processes.Default.Command='curl --proxy 127.0.0.1:{0} "http://www.example.com/remap" --verbose'.format(ts.Variables.port)
+tr.Processes.Default.Command='curl --proxy 127.0.0.1:{0} "http://www.example.com/{1}" --verbose'.format(ts.Variables.port,Test.testName)
 tr.Processes.Default.ReturnCode=0
 tr.Processes.Default.Streams.All="gold/remap-200.gold"
 
 # www.example.com:80 host
 tr=Test.AddTestRun()
-tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.example.com:80/remap" --verbose'.format(ts.Variables.port)
+tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.example.com:80/{1}" --verbose'.format(ts.Variables.port,Test.testName)
 tr.Processes.Default.ReturnCode=0
 tr.Processes.Default.Streams.All="gold/remap-200.gold"
 
 # www.example.com:8080 host
 tr=Test.AddTestRun()
-tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.example.com:8080/remap" --verbose'.format(ts.Variables.port)
+tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.example.com:8080/{1}" --verbose'.format(ts.Variables.port,Test.testName)
 tr.Processes.Default.ReturnCode=0
 tr.Processes.Default.Streams.All="gold/remap-200.gold"
 
 # no rule for this
 tr=Test.AddTestRun()
-tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.test.com/remap" --verbose'.format(ts.Variables.port)
+tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.test.com/{1}" --verbose'.format(ts.Variables.port, Test.testName)
 tr.Processes.Default.ReturnCode=0
 tr.Processes.Default.Streams.All="gold/remap-404.gold"
 
 # bad port
 tr=Test.AddTestRun()
-tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.example.com:1234" --verbose'.format(ts.Variables.port)
+tr.Processes.Default.Command='curl  --proxy 127.0.0.1:{0} "http://www.example.com:1234/{1}" --verbose'.format(ts.Variables.port,Test.testName)
 tr.Processes.Default.ReturnCode=0
 tr.Processes.Default.Streams.All="gold/remap-404.gold"
 
